bb00671002bf7b45c7030d13a7e05367a2bd1c9c
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 12:35:34 +0000

    updates

--- components/Sites/index.js
+++ components/Sites/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, GridItem, NerdGraphQuery, Spinner} from 'nr1'
+import { Grid, GridItem, NerdGraphQuery, Spinner, SparkLine, LineChart} from 'nr1'
 import PropTypes from 'prop-types';
 
 export default class Sites extends Component {
@@ -12,6 +12,7 @@ export default class Sites extends Component {
     async componentDidMount() {
         this.loadData()
     }
+   
 
     loadData() {
 
@@ -19,15 +20,15 @@ export default class Sites extends Component {
             id: 692732 //eagleeye account id
         }
             
-        let query = `
-                query($id: Int!) {
+
+        let query = `query($id: Int!) {
                     actor {
                         account(id: $id) {
                                 ${this.buildQueryObject()}    
                             }
                     }
                 }
-           `
+           `       
         console.log(query);
         const q = NerdGraphQuery.query({ query: query, variables: variables })
 
@@ -36,6 +37,7 @@ export default class Sites extends Component {
             let resultsObj={}
             this.props.config.domain.forEach((domain,idx)=>{
                 resultsObj[`site_${idx}`]=results.data.actor.account[`site_${idx}`].results
+                resultsObj[`site_${idx}_plseries`]=results.data.actor.account[`site_${idx}_plseries`].results
             })
             
             //set the state with the data from the query
@@ -47,12 +49,41 @@ export default class Sites extends Component {
     {
         let domains = this.props.config.domain; 
         let domainQueries = ''; 
+        
+        // const { duration } = this.props.nr1.launcher.state.timeRange;
+        // const durationInMinutes = duration / 1000 / 60;
+        const durationInMinutes = 60 
         domains.forEach((domain, index) => {
-            domainQueries += `site_${index}: nrql(query: "SELECT average(duration) as 'AvgDuration' from PageView where domain='${domain}' ") {results}`
+            domainQueries += `site_${index}: nrql(query: "SELECT average(duration) as 'AvgDuration' from PageView where domain='${domain}' since ${durationInMinutes} minutes ago") {results}`
+            domainQueries += `site_${index}_plseries: nrql(query: "SELECT average(duration) as 'AvgDuration' from PageView where domain='${domain}' timeseries since ${durationInMinutes} minutes ago") {results}`
         });
 
         return domainQueries;
     }    
+
+    convertChartData(data,fieldName) {
+        const seriesData=data.map(i => { 
+            i.x = i.endTimeSeconds
+            i.y = i[fieldName]
+            return i
+        });
+        const graphData = [
+            {
+                metadata: {
+                    id: 'series-1',
+                    name: this.props.title,
+                    color: '#f41b1b',
+                    viz: 'main',
+                    units_data: {
+                        x: 'TIMESTAMP',
+                        y: 'TRANSACTIONS',
+                    }
+                },
+                data: seriesData
+            }
+        ];
+        return graphData
+    }
 
     render() {
 
@@ -65,6 +96,9 @@ export default class Sites extends Component {
             console.log("data",data)
             let sites=[]
             config.domain.forEach((domain,idx)=>{
+
+            let seriesData=this.convertChartData(data[`site_${idx}_plseries`],"AvgDuration")
+            console.log(seriesData)
             sites.push(
                 <Grid key={domain}>
                     <GridItem columnSpan={3}>
@@ -72,7 +106,9 @@ export default class Sites extends Component {
                     </GridItem>
                     <GridItem className="LargeNumber" columnSpan={3}>{Number(data[`site_${idx}`][0].AvgDuration).toFixed(2)}
                     <div className="LargeNumberTitle">Average PageLoad</div></GridItem>
-                    <GridItem columnSpan={3}><div>Three</div></GridItem>
+                    <GridItem columnSpan={3}>
+                        <LineChart  data={seriesData} /> {/* <SparkLine data={seriesData}/>  */}test
+                    </GridItem>
                     <GridItem className="grey" columnSpan={3}><div>Four</div></GridItem>
                 </Grid>
             ) 
--- nerdlets/workshop-nerdlet/index.js
+++ nerdlets/workshop-nerdlet/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from "../../components/Header"
-import { Grid, GridItem} from 'nr1'
+import { Grid, GridItem, NerdletStateContext, PlatformStateContext} from 'nr1'
 import Sites from "../../components/Sites"
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class WorkshopNerdletNerdlet extends React.Component {
+    
+    async componentDidUpdate(){
+        let { launcherUrlState } = this.props
+        let newTimeRange = launcherUrlState.timeRange
+    }
+
     render() {
 
         const config={
@@ -16,13 +22,22 @@ export default class WorkshopNerdletNerdlet extends React.Component {
             ]
         }
 
-        return <>
-            <Grid>
-                <GridItem className="headerRow" columnSpan={12}>
-                    <Header title="Eagle Eye Dashboard" subtitle="All the apps in one place" />
-                </GridItem >
-            </Grid>
-            <Sites config={config}/>
-        </>
+        return <PlatformStateContext.Consumer>
+            {(launcherUrlState) => (
+              <NerdletStateContext.Consumer>
+                {(nerdletUrlState) => (
+                    <>
+                    <Grid>
+                    <GridItem className="headerRow" columnSpan={12}>
+                        <Header title="Eagle Eye Dashboard" subtitle="All the apps in one place" />
+                    </GridItem >
+                    </Grid>
+                    <Sites config={config}/>
+                    </>
+                )}
+                </NerdletStateContext.Consumer>
+            )}
+        </PlatformStateContext.Consumer>
+
     }
 }
