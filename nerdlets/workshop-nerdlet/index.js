092b3a0049e72e1b6ed4b7e1426fd5b53f9c9f18
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 10:30:38 +0000

    updates

--- nerdlets/workshop-nerdlet/index.js
+++ nerdlets/workshop-nerdlet/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BarChart} from 'nr1'
+import { Grid, GridItem} from 'nr1'
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class WorkshopNerdletNerdlet extends React.Component {
     render() {
 
-        return <BarChart
-            accountId={692732}
-            query='SELECT count(*) FROM `Transaction` SINCE 1 DAY AGO'
-        />;
+        return <>
+            <Grid>
+                <GridItem columnSpan={12}><div>Header</div></GridItem>
+            </Grid>
+            <Grid>
+                <GridItem columnSpan={3}><div>One</div></GridItem>
+                <GridItem columnSpan={3}><div>Two</div></GridItem>
+                <GridItem columnSpan={3}><div>Three</div></GridItem>
+                <GridItem columnSpan={3}><div>Four</div></GridItem>
+            </Grid>
+        </>
     }
 }
