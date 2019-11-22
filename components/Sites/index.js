452a4921a7d69a9d3bc0d3a3690cf116b0bfae18
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 11:17:51 +0000

    updates

--- components/Sites/index.js
+++ components/Sites/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, GridItem} from 'nr1'
 import PropTypes from 'prop-types';
 
 export default class Sites extends Component {
@@ -6,13 +7,27 @@ export default class Sites extends Component {
     render() {
 
        // const { title, subtitle } = this.props;
+       const config={
+        domain: ["www.prezzoegifts.co.uk",
+            "gigglingsquidgift.com",
+            "www.vintageinngifts.co.uk",
+            "www.sizzlingpubgifts.co.uk"
+        ]
+    }
+    
+        let sites=[]
+        config.domain.forEach((domain)=>{
+        sites.push(
+            <Grid key={domain}>
+                <GridItem className="grey" style={{"border":"solid 1px red"}} columnSpan={3}><div>One</div></GridItem>
+                <GridItem className="grey" columnSpan={3}><div>Two</div></GridItem>
+                <GridItem className="grey" columnSpan={3}><div>Three</div></GridItem>
+                <GridItem className="grey" columnSpan={3}><div>Four</div></GridItem>
+            </Grid>
+        ) 
+        })
 
-        return  <>
-            <div>
-                <h1>{title}</h1>
-                <div className="subtitle">{subtitle}</div>
-            </div>
-        </>
+        return  <>{sites}</>
         
     }
 }
--- nerdlets/workshop-nerdlet/index.js
+++ nerdlets/workshop-nerdlet/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, GridItem} from 'nr1'
 import Header from "../../components/Header"
+import { Grid, GridItem} from 'nr1'
 import Sites from "../../components/Sites"
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
@@ -8,25 +8,6 @@ import Sites from "../../components/Sites"
 export default class WorkshopNerdletNerdlet extends React.Component {
     render() {
 
-        const config={
-            domain: ["www.prezzoegifts.co.uk",
-                "gigglingsquidgift.com",
-                "www.vintageinngifts.co.uk",
-                "www.sizzlingpubgifts.co.uk"
-            ]
-        }
-        
-        let sites=[]
-        config.domain.forEach((domain)=>{
-           sites.push(
-            <Grid key={domain}>
-                <GridItem className="grey" style={{"border":"solid 1px red"}} columnSpan={3}><div>One</div></GridItem>
-                <GridItem className="grey" columnSpan={3}><div>Two</div></GridItem>
-                <GridItem className="grey" columnSpan={3}><div>Three</div></GridItem>
-                <GridItem className="grey" columnSpan={3}><div>Four</div></GridItem>
-            </Grid>
-           ) 
-        })
 
         return <>
             <Grid>
@@ -34,7 +15,7 @@ export default class WorkshopNerdletNerdlet extends React.Component {
                     <Header title="Eagle Eye Dashboard" subtitle="All the apps in one place" />
                 </GridItem>
             </Grid>
-            {sites}
+            <Sites />
         </>
     }
 }
