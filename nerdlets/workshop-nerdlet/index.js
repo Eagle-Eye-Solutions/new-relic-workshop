144523aec5803a27a227acc421af66f2fa869157
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 10:20:30 +0000

    updates

--- nerdlets/workshop-nerdlet/index.js
+++ nerdlets/workshop-nerdlet/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BarChart} from 'nr1'
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class WorkshopNerdletNerdlet extends React.Component {
     render() {
-        return <h1>Hello, workshop-nerdlet Nerdlet!</h1>;
+
+        return <BarChart
+            accountId={692732}
+            query='SELECT count(*) FROM `Transaction` SINCE 1 DAY AGO'
+        />;
     }
 }
