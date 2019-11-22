8f4dfd3d0e254eba97d5c6b4dde2fdebcda07bfd
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 10:46:33 +0000

    updates

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,10 +6,12 @@ export default class Header extends Component {
 
     render() {
 
+        const { title, subtitle } = this.props;
+
         return  <>
             <div>
-                <h1>{this.props.title}</h1>
-                <div>{this.props.subtitle}</div>
+                <h1>{title}</h1>
+                <div>{subtitle}</div>
             </div>
         </>
         
