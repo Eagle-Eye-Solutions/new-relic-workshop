0af001f533465054413bac5a8cef16ef79d8106e
James Buchanan <jbuchanan@newrelic.com>
2019-11-22 13:00:53 +0000

    updates

--- nerdlets/workshop-nerdlet/index.js
+++ nerdlets/workshop-nerdlet/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../../components/Header"
 import { Grid, GridItem, NerdletStateContext, PlatformStateContext, Icon} from 'nr1'
 import Sites from "../../components/Sites"
+import { Map, Marker, Popup, TileLayer, CircleMarker } from 'react-leaflet'
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
@@ -30,11 +31,27 @@ export default class WorkshopNerdletNerdlet extends React.Component {
             ]
         }
 
+        const position = [51.505, -0.09]
+        const map = (
+        <Map style={{ height: '200px' }} center={position} zoom={4}>
+            <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <CircleMarker center={position} radius={30} color='#ff00ff' />
+            <Marker position={position}>
+                <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+            </Marker>
+        </Map>
+        )
+
+
         return <PlatformStateContext.Consumer>
             {(launcherUrlState) => (
               <NerdletStateContext.Consumer>
                 {(nerdletUrlState) => (
                     <>
+                    {map}
                     <Grid>
                     <GridItem className="headerRow" columnSpan={8}>
                         <Header title="Eagle Eye Dashboard" subtitle="All the apps in one place" />
